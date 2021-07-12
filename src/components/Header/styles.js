2b3149611bba2933995c4c685822d6ffa5833dbf
Kalin1n <kalinin_ivan_donetsk@ukr.net>
2021-07-12 21:51:32 +0300

    change favicon

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,28 +1,27 @@
-import {makeStyles} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
-
-const useStyles = makeStyles(theme =>({
- header:{
-  width: '100%',
-  padding: '30px',
-  display:"flex",
-  justifyContent: 'space-between',
-  alignItems: 'center'
- },
- actionsWrapper:{
-  width: '50%'
- },
- button:{
-  width:"100px",
-  margin:"0px 20px",
-  background: theme.palette.primary.secondary,
-  color: "#fff"
- },
- input:{
-  height: '30px',
-  width: "50%",
-  padding: '5px'
- }
+const useStyles = makeStyles((theme) => ({
+  header: {
+    width: "100%",
+    padding: "30px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  actionsWrapper: {
+    width: "50%",
+  },
+  button: {
+    width: "100px",
+    margin: "0px 20px",
+    background: theme.palette.primary.secondary,
+    color: "#fff",
+  },
+  input: {
+    height: "30px",
+    width: "50%",
+    padding: "5px",
+  },
 }));
 
 export default useStyles;
